3e22fab37879a5852d0514a6982cce90c44ea10d
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2022-07-22 02:57:54 +0300

    json obj. silindi apiden response search geldi

--- src/App.js
+++ src/App.js
@@ -6,27 +6,7 @@ import MovieList from './components/MovieList';
 
 
 const App = () => {
-  const [movies, setMovies] = useState([{
-    "Title": "Star Wars",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-},]);
+  const [movies, setMovies] = useState([]);
 
 const getMovieRequest = async () => {
   const url = "http://www.omdbapi.com/?s=star wars&apikey=15c87b94" //api url si değişecek star wars silinip search value eklenecek.
@@ -34,6 +14,7 @@ const getMovieRequest = async () => {
   const responseJson = await response.json();
 
   console.log(responseJson);
+  setMovies(responseJson.Search)
 };
 
 

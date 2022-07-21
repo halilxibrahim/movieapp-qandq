342f1765c17414027f317de2df5808cb217bf963
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2022-07-22 02:14:40 +0300

    usaState ve filmler eklendi. Dosyalar düzenlendi.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [movies, setMovies] = useState([{
+    "Title": "Star Wars",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+},]);
 
-export default App;
+  return <div> Merhaba deneme</div>
+};
+
+export default App; 
